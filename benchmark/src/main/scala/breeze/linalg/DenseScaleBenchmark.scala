51e74026afff22eecaeca19a77034b5df4c33a5f
Brian Wignall <brianwignall@gmail.com>
2024-06-27 06:36:16 -0400

    Remove unused

--- benchmark/src/main/scala/breeze/linalg/DenseScaleBenchmark.scala
+++ benchmark/src/main/scala/breeze/linalg/DenseScaleBenchmark.scala
@@ -1,6 +1,6 @@
 package breeze.linalg
 
-import breeze.benchmark.{MyRunner, BreezeBenchmark}
+import breeze.benchmark.{BreezeBenchmark, MyRunner}
 import breeze.macros._
 
 /**
@@ -13,14 +13,14 @@ class DenseScaleBenchmark extends BreezeBenchmark {
 //  val dv, dv2 = DenseVector.rand(100000).apply(0 to -1 by 2)
 
   def timeSmallDVScale(reps: Int): DenseVector[Double] = {
-    cforRange(0 until reps) { rep =>
+    cforRange(0 until reps) { _ =>
       dv *= 1.0001
     }
     dv
   }
 
   def timeSmallDVInlineRange(reps: Int): DenseVector[Double] = {
-    cforRange(0 until reps) { rep =>
+    cforRange(0 until reps) { _ =>
       val ad = dv.data
       cforRange(0 until dv.length) { i =>
         ad(i) *= 1.0001
@@ -31,7 +31,7 @@ class DenseScaleBenchmark extends BreezeBenchmark {
 
   def timeSmallDVScaleInline(reps: Int): DenseVector[Double] = {
     val d = dv.data
-    cforRange(0 until reps) { rep =>
+    cforRange(0 until reps) { _ =>
       d(0) *= 1.0001
       d(1) *= 1.0001
       d(2) *= 1.0001
