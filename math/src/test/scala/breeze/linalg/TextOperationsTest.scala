c02312cac5ed699619b86aca666d2828672332e9
Brian Wignall <brianwignall@gmail.com>
2024-06-29 20:00:05 -0400

    Fix unit test

--- math/src/test/scala/breeze/linalg/TextOperationsTest.scala
+++ math/src/test/scala/breeze/linalg/TextOperationsTest.scala
@@ -3,18 +3,18 @@ package breeze.linalg
 import java.io.File
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.nio.file.{Path, Paths}
+
 /**
  * Created by Luca Puggini: lucapuggio@gmail.com on 19/02/16.
  */
 class TextOperationsTest extends AnyFunSuite {
   test("csvread and String2File methods") {
     // A csv file can be read both using the java File function and the toFile method of the string class
-    val file_path = if (new File(".").getAbsolutePath.endsWith("math/.")) {
-      "src/test/resources/glass_data.txt"
-    } else {
-      "./math/src/test/resources/glass_data.txt"
-    }
-    val csv1 = csvread(new File(file_path))
+    val url = getClass.getClassLoader.getResource("glass_data.txt")
+    val file_path: Path = Paths.get(url.toURI)
+
+    val csv1 = csvread(new File(file_path.toString))
     val csv2 = csvread(file_path.toFile)
     assert(csv1 == csv2)
   }
